397da34f6817fa3539301746bcc07d50e463c747
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-04-09 15:58:39 -0400

    start of model draft

---
+++ models/bechdel_movies.js
@@ -0,0 +1,14 @@
+// Draft SQL searches
+
+// Filter movies by search word from form input (ex below with star)
+// SELECT * FROM bechdel_movies WHERE title LIKE '%Star%' or title LIKE '%star%';
+
+// Sort movies by year
+// SELECT * FROM bechdel_movies WHERE yr = '1980';
+
+
+"use strict";
+
+const db = require("../db");
+
+class bechdel 
