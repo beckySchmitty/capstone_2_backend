87c2671ac05fb23b5d37ba15504d6ea32f84b9da
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-04-22 10:03:19 -0400

    fixed api login issue

--- models/User.js
+++ models/User.js
@@ -2,28 +2,22 @@
 
 const db = require("../db");
 const bcrypt = require("bcrypt");
-const ExpressError = require("../expressError");
-const saltRounds = 10;
+const {ExpressError, NotFoundError} = require("../expressError");
 const { BCRYPT_WORK_FACTOR } = require("../config");
 const { v4: uuidv4 } = require('uuid');
 
-
-
 class User {
-
 
 //  Register user
 // {username, password, email, img_url}
   static async register({username, password, email, img_url}) { 
-    // const salt = bcrypt.genSaltSync(saltRounds);
-    // const hashedPwd = bcrypt.hashSync(password, salt);    
+    let hashedPwd = await bcrypt.hash(password, BCRYPT_WORK_FACTOR)    
     let id = uuidv4();
-    console.log(`^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^${username, password, email, img_url}`)
     const results = await db.query(`INSERT INTO users
     (id, username, password, email, img_url)
     VALUES ($1, $2, $3, $4, $5) 
     RETURNING id, username, password, email, img_url`
-    , [id, username, password, email, img_url]);
+    , [id, username, hashedPwd, email, img_url]);
 
     return results.rows[0];
   }
@@ -43,7 +37,7 @@ class User {
 
   static async get(username) {
     const userRes = await db.query(
-          `SELECT username, password, email, img_url
+          `SELECT id, username, email, img_url
            FROM users
            WHERE username = $1`,
         [username],
