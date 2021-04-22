d334afdf65854a097e6c668a586f218e3bd38598
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-04-21 22:44:24 -0400

    searching for 500 error

--- routes/user.js
+++ routes/user.js
@@ -14,6 +14,7 @@ router.get('/hi', (req, res, next) => {
 // {username, password, email, img_url} => token
 router.post('/register', async (req, res, next) => {
   try {
+    console.log(`@@@@@@@@@@ REQ BODY ${JSON.stringify(req)}`)
     let {username, password, email, img_url} = req.body;
     const user = await User.register({username, password, email, img_url})
 
